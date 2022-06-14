d492ac2af2286537d523fdc97bd8c5178e6b6cd3
Esen <esenbekm03@gmail.com>
2022-06-14 17:34:59 +0600

    lesson 3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from aiogram.utils import executor
-from handlers import client, callback, extra, admin
+from handlers import client, callback, extra, admin, fsm_anketa
 
 from config import dp
 import logging
@@ -7,6 +7,7 @@ import logging
 client.register_handler_client(dp)
 callback.register_handlers_callback(dp)
 admin.register_handler_admin(dp)
+fsm_anketa.register_handler_fsmanketa(dp)
 
 extra.register_handlers_extra(dp)
 
